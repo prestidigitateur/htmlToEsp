38f33e1b494482fc29ab3a84a81e04edff9db536
Dmitriy Grid <prestidigitateur.VS@gmail.com>
2018-02-18 23:02:16 +0300

    Release

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# htmlToEsp
+## about
+> Convert your html page to one-line Esp8266 compatible form.
+
+[online](https://prestidigitateur.github.io/htmlToEsp)
+
+This web application is written to automate site migration (html, css, js*) to Esp8266.
+Insert your html code and click "convert", your code will remove line breaks and _"_ will be replaced with _\"_.
+In the same way, you can convert styles and JavaScript.
+!Important, your JavaScript should not have comments like: "//comment", otherwise when deleting the line breaks the first comment will cut off all further code.
---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+﻿function convertCode(){
+    var s = $("#sourse").val();
+    s = s.replace(/(\r\n|\n|\r)/gm," ");
+    s = s.replace(/"/g, '\\"');
+    s = s.replace(/\s+/g," ");
+    $("#compressed").val(s);
+    $("#e").click();
+}
+
+function restoreCode(){
+    var s = $("#compressed").val();
+    s = s.replace(/>/g,'> \n');
+    s = s.replace(/\\\"/g, '\"');
+    $("#sourse").val(s);
+    $("#h").click();
+}
+function copyCode(s){
+    $("#"+s).select().focus();
+    document.execCommand("copy");
+    $("#"+s).blur();
+}
+function copyCodeAsString(s){
+    $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val("String myHtml = \"" + $("#"+s).val() + "\";").select().focus();
+    document.execCommand("copy");
+    $temp.remove();
+    $("#"+s).focus();
+}
+function clearCode(s){
+    $("#"+s).val("");
+}
+
+function onFileDrop(e) {      
+      var file = e.dataTransfer.files[0];
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        var textFromFileLoaded = event.target.result;
+        $('#sourse').val(textFromFileLoaded);
+      }
+      reader.readAsText(file);
+      e.preventDefault();
+}
